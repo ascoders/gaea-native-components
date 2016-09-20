a8e3281b28e3d8447fd317a88d0851d281229fc8
huangziyi01 <huangziyi01@baidu.com>
2016-09-20 20:38:46 +0800

    开发

---
+++ lib/gaea-native-components/gaea-native-components.component.d.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as typings from './gaea-native-components.type';
+export default class GaeaNativeComponents extends React.Component<typings.PropsDefine, typings.StateDefine> {
+    static defaultProps: typings.PropsDefine;
+    state: typings.StateDefine;
+    render(): JSX.Element;
+}
---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import GaeaNativeComponents from './gaea-native-components/gaea-native-components.component';
+import { PropsDefine as GaeaNativeComponentsPropsDefine } from './gaea-native-components/gaea-native-components.type';
+export { GaeaNativeComponents, GaeaNativeComponentsPropsDefine };
+export default GaeaNativeComponents;
