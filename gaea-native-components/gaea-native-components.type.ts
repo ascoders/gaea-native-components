385c8302a485adb558ab55dd726d93eb746d370d
huangziyi01 <huangziyi01@baidu.com>
2016-09-20 20:18:57 +0800

    add gaea

---
+++ gaea-native-components/gaea-native-components.type.ts
@@ -0,0 +1,25 @@
+
+                    import * as React from 'react'
+
+                    export interface PropsDefine {
+
+                    }
+                    
+                    export class PropsGaea {
+                        gaeaName = '盖亚-原生组件'
+                        gaeaIcon = 'square-o'
+                        gaeaUniqueKey = 'nt-editor-gaea-native-components'
+                    }
+                    
+                    export class Props extends PropsGaea implements PropsDefine {
+                        
+                    }
+                    
+                    export interface StateDefine {
+                    
+                    }
+                    
+                    export class State implements StateDefine {
+                    
+                    }
+                
---
+++ index.ts
@@ -0,0 +1,7 @@
+
+                    import GaeaNativeComponents from './gaea-native-components/gaea-native-components.component'
+                    import {PropsDefine as GaeaNativeComponentsPropsDefine} from './gaea-native-components/gaea-native-components.type'
+
+                    export {GaeaNativeComponents, GaeaNativeComponentsPropsDefine}
+                    export default GaeaNativeComponents
+                
