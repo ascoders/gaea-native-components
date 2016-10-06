695ad3b83e51245e3e83efc908b7b8b82d223796
huangziyi01 <huangziyi01@baidu.com>
2016-10-06 14:28:48 +0800

    开发

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
-import View from './gaea-native-components/view'
-import Text from './gaea-native-components/text'
-import TextInput from './gaea-native-components/text-input'
-import Switch from './gaea-native-components/switch'
+import View from './view/view.component'
+// import Text from './gaea-native-components/text'
+// import TextInput from './gaea-native-components/text-input'
+// import Switch from './gaea-native-components/switch'
 
-export default [View, Text, TextInput, Switch] as any
+export default [
+    View,
+    // Text,
+    // TextInput,
+    // Switch
+] as any
---
+++ view/view.type.tsx
@@ -0,0 +1,47 @@
+import {TransparentlyPropsPropsDefine} from '../../../common/transparently-props/index'
+import {gaeaHelper} from '../../gaea-helper/index'
+
+export interface PropsDefine extends TransparentlyPropsPropsDefine {
+
+}
+
+export class PropsGaea {
+    gaeaName = '布局'
+    gaeaIcon = 'square-o'
+    gaeaUniqueKey = 'gaea-layout'
+    gaeaEdit = [
+        '布局',
+        gaeaHelper.layoutEditor,
+        gaeaHelper.marginPaddingEditor,
+        gaeaHelper.widthHeightEditor,
+        gaeaHelper.overflowEditor,
+        '背景',
+        gaeaHelper.backgroundEditor,
+        '特效',
+        gaeaHelper.opacityEditor
+    ]
+}
+
+export class Props extends PropsGaea implements PropsDefine {
+    style = Object.assign(
+        {},
+        gaeaHelper.layout,
+        gaeaHelper.marginPadding,
+        gaeaHelper.opacity,
+        gaeaHelper.widthHeight,
+        gaeaHelper.overflow,
+        gaeaHelper.background,
+        {
+            display: 'flex',
+            backgroundColor: 'white'
+        }
+    )
+}
+
+export interface StateDefine {
+
+}
+
+export class State implements StateDefine {
+
+}
