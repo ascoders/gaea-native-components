6ce0a93fb392a85e4e4f8623a47d6f39a1d715ad
huangziyi01 <huangziyi01@baidu.com>
2016-10-10 19:20:35 +0800

    发布组件

--- lib/gaea-native-components/view/index.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as module from './module';
-export default class LViewComponent extends React.Component<module.PropsInterface, module.StateInterface> {
-    static defaultProps: module.PropsInterface;
-    state: module.StateInterface;
-    render(): JSX.Element;
-}
