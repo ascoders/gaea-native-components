764684556c3963790f98f264faaafd827128c646
huangziyi01 <huangziyi01@baidu.com>
2016-11-27 15:16:57 +0800

    发布组件

--- lib/switch/switch.component.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as typings from './switch.type';
-export default class SwitchComponent extends React.Component<typings.PropsDefine, typings.StateDefine> {
-    static defaultProps: typings.PropsDefine;
-    state: typings.StateDefine;
-    render(): JSX.Element;
-}
