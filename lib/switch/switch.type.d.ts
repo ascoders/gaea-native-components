173b32e5a69ffc8339f1f5b5d14e614786087415
huangziyi01 <huangziyi01@baidu.com>
2016-11-27 15:23:51 +0800

    发布组件

---
+++ lib/switch/switch.type.d.ts
@@ -0,0 +1,25 @@
+import { TransparentlyNativePropsPropsDefine } from  'nt-transparently-native-props';
+import * as ReactNaitve from 'react-native';
+export interface PropsDefine extends TransparentlyNativePropsPropsDefine {
+    style?: ReactNaitve.ViewStyle;
+    checked?: boolean;
+}
+export declare class PropsGaea {
+    gaeaName: string;
+    gaeaIcon: string;
+    gaeaUniqueKey: string;
+    gaeaEdit: (string | {
+        field: any;
+        label: string;
+        editor: string;
+        editable: boolean;
+    })[];
+}
+export declare class Props extends PropsGaea implements PropsDefine {
+    style: any;
+    checked: boolean;
+}
+export interface StateDefine {
+}
+export declare class State implements StateDefine {
+}
