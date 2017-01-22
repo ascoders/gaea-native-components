d324d6cd39b0ae6ddbafff2f3f90177b552ef6e3
huangziyi01 <huangziyi01@baidu.com>
2017-01-22 16:34:33 +0800

    发布组件

--- lib/text-input/text-input.component.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as typings from './text-input.type';
-export default class TextInputComponent extends React.Component<typings.PropsDefine, typings.StateDefine> {
-    static defaultProps: typings.PropsDefine;
-    state: typings.StateDefine;
-    render(): JSX.Element;
-}
